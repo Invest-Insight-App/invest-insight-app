ec71a0f3d64b3c766aff2a182734c6840f0678cb
lucienlmp <lucienlmp@hotmail.com>
2024-04-20 23:56:07 +0100

    selection page

--- app/market-options/page.tsx
+++ app/market-options/page.tsx
@@ -4,36 +4,76 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <div className="bg-white my-8 p-6 mb-0">
-        <h2 className="boldtitle">Process</h2>
-        <div className="grid grid-cols-6">
-          <div className="col-span-1 p-8">
-            <Image
-              alt="process-verticle"
-              src="/process_3_verticle.png"
-              width={80}
-              quality={100}
-              height={15}
-            />
-          </div>
-          <div className="col-span-5 grid grid-rows-3 items-center">
-            <p className="process-text">
+      <form>
+        <div className="bg-white my-8 p-6 mb-0">
+          <div>
+            <h2 className="process-text">Step 1</h2>
+            <label htmlFor="market" className="boldtitle text-primary">
               {" "}
-              Pick a market, company and time-scale
-            </p>
-            <p className="process-text">
-              Choose the report or stock-picking suggestion through AI sentiment
-              analysis
-            </p>
-            <p className="process-text"> Get your result!</p>
+              What market are you interested in finding out about?
+            </label>
+            <div className="px-16 py-8">
+              <select
+                id="market"
+                className="x-xl bf-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              >
+                <option selected>Choose a market</option>
+                <option value="stock">Stock market</option>
+                <option value="bond">Bond market</option>
+                <option value="crypto">Cryptocurrency</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="process-text">Step 1</h2>
+            <label htmlFor="market" className="boldtitle text-primary">
+              {" "}
+              What market are you interested in finding out about?
+            </label>
+            <div className="px-16 py-8">
+              <select
+                id="market"
+                className="x-xl bf-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              >
+                <option selected>Choose a market</option>
+                <option value="stock">Stock market</option>
+                <option value="bond">Bond market</option>
+                <option value="crypto">Cryptocurrency</option>
+              </select>
+            </div>
+          </div>
+
+          <div>
+            <h2 className="process-text">Step 1</h2>
+            <label htmlFor="market" className="boldtitle text-primary">
+              {" "}
+              What market are you interested in finding out about?
+            </label>
+            <div className="px-16 py-8">
+              <select
+                id="market"
+                className="x-xl bf-white border border-gray-300 text-gray-900 text-xl rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+              >
+                <option selected>Choose a market</option>
+                <option value="stock">Stock market</option>
+                <option value="bond">Bond market</option>
+                <option value="crypto">Cryptocurrency</option>
+              </select>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="p-6 bg-white">
-        <Link href="/tickets">
-          <button className="btn-large p-0 m-0">
-            Yup, I'm ready to start!
+      </form>
+      <div className="p-6 bg-white grid grid-cols-5">
+        <Link href="/" className="col-start-1 col-end-3">
+          <button className="btn-large p-0 m-0 col-start-1 col-end-2">
+            {" "}
+            Previous{" "}
           </button>
+        </Link>
+
+        <Link href="/type-options" className="col-start-4 col-end-6">
+          <button className="btn-large p-0 m-0  "> Next </button>
         </Link>
       </div>
     </main>

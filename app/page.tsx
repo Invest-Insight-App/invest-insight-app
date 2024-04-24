c6087697be75dc35a3c37a943662c8b03338cde6
lucienlmp <lucienlmp@hotmail.com>
2024-04-24 22:51:35 +0100

    change button text

--- app/page.tsx
+++ app/page.tsx
@@ -46,7 +46,9 @@ export default function Home() {
       </div>
       <div className="p-6 bg-white">
         <Link href="/market-options">
-          <button className="btn-large p-0 m-0">Do another search!</button>
+          <button className="btn-large p-0 m-0">
+            Yup, I'm ready to start!
+          </button>
         </Link>
       </div>
     </main>

25ae6771e578c8b9056660a8eaabcc51bf1848c7
environmentallyconsciouscoder <environmentallyconsciouscoder@gmail.com>
2024-04-25 01:20:01 +0100

    chore: added initial data for company

---
+++ app/data.ts
@@ -0,0 +1,19 @@
+export const initialCompaniesData = [{
+    category: null,
+    cik: null,
+    currency: null,
+    cusip: null,
+    exchange: null,
+    fama_industry: null,
+    fama_sector: null,
+    id: null,
+    industry: null,
+    is_delisted: null,
+    location: null,
+    name: null,
+    sector: null,
+    sic: null,
+    sic_industry: null,
+    sic_sector: null,
+    ticker: null
+}];
